8eae906a7436ca19357359532859f00500a3c561
ChoiAnYong <dksdyd78@naver.com>
2025-06-14 15:14:59 +0900

    feat: 프로젝트 세팅

---
+++ PicMate/PicMate/Presentation/Scene/PhotoScene/View/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PicMate
+//
+//  Created by 최안용 on 6/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
