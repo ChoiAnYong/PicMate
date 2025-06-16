de7004ede73b066f2872c51e3964fc22007dca28
ChoiAnYong <dksdyd78@naver.com>
2025-06-16 14:56:25 +0900

    feat: 임시 뷰컨 구현

---
+++ PicMate/PicMate/Presentation/Scene/CleanUpScene/View/CleanUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CleanUpViewController.swift
+//  PicMate
+//
+//  Created by 최안용 on 6/16/25.
+//
+
+import UIKit
+
+final class CleanUpViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .blue
+    }
+}
---
+++ PicMate/PicMate/Presentation/Scene/FullPhotoScene/View/FullPhotoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FullPhotoViewController.swift
+//  PicMate
+//
+//  Created by 최안용 on 6/16/25.
+//
+
+import UIKit
+
+final class FullPhotoViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
+}
