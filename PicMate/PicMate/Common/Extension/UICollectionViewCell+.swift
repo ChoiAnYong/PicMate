98e88bc38a95ce1aae9b25e70e284b94e08bcd31
ChoiAnYong <dksdyd78@naver.com>
2025-06-18 21:16:59 +0900

    feat: UICollectionViewCell Indentifier 구현

---
+++ PicMate/PicMate/Common/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  PicMate
+//
+//  Created by 최안용 on 6/17/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+}
